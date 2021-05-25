8c62f696de0cd48e56724dfed718fee8bafccc6a
Yoann Vigouroux <yoann.vigouroux.10@gmail.com>
2021-05-25 12:35:29 +0200

    Implemented spectral clustering method

---
+++ meetings-speaker-diarization/SpeakerDiarizer.py
@@ -0,0 +1,44 @@
+import torch
+import torchaudio
+from spectralcluster import SpectralClusterer
+import numpy as np
+
+class SpeakerDiarizer:
+  
+  # Attributes
+  wav2mel = torch.jit.load("data/pretrained_models/wav2mel.pt")
+  dvector = torch.jit.load("data/pretrained_models/dvector-step250000.pt").eval()
+  
+  def __init__(self):
+    self.sample_rate = None
+    self.wav_tensor = None
+    self.emb_tensor = None
+    self.speaker_label = None
+  
+  def load(self, audio_file_path):
+    self.wav_tensor, self.sample_rate = torchaudio.load(audio_file_path)
+  
+  def generate_dvectors(self):
+    # nb_windows = audio without silence / frame_rate
+    mel_tensor = self.wav2mel(self.wav_tensor, self.sample_rate)  # shape: (nb_windows, n_mels)
+    emb_tensor = self.dvector.embed_utterance(mel_tensor)  # shape: (emb_dim)
+    
+    self.emb_tensor = emb_tensor
+    
+  def spectral_clustering(self):
+    
+    clusterer = SpectralClusterer(min_clusters=1,
+                                  max_clusters=100,
+                                  p_percentile=0.95,
+                                  gaussian_blur_sigma=1)
+    embedded_input = []
+    
+    for i in range(len(self.emb_tensor)):
+
+      embedded_input.append(self.emb_tensor[i].detach().numpy())
+      
+    embedded_input = np.array(embedded_input)
+    
+    speaker_label = clusterer.predict(embedded_input)
+    
+    self.speaker_label = speaker_label
