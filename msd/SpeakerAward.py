67326112733ab8f4eaaaac27ca63096ea8a69d04
Martin Jauffret <martinjfrt@hotmail.fr>
2021-05-31 12:43:57 +0200

    Name Features Implement

--- msd/SpeakerAward.py
+++ msd/SpeakerAward.py
@@ -13,26 +13,20 @@ class SpeakerAward(SpeakerDiarizer):
     load_dotenv()
     
     #Attributes
-    speech_config = speechsdk.SpeechConfig(subscription=os.getenv('AZURE_API_KEY'), region="francecentral", speech_recognition_language = 'en-US')
+    load_dotenv()
+    speech_config = speechsdk.SpeechConfig(subscription=os.getenv('AZURE_API_KEY'), region="francecentral", speech_recognition_language = 'fr-FR')
     
     def __init__(self, name_pipe):
-        # , pipeline, diarization, der, current_filename
-    
         self.json_outputs = None
-        super().__init__(name_pipe)
-        self.pipeline = torch.hub.load('pyannote/pyannote-audio', self.name_pipe)
-        self.diarization = None
-        self.der = None
-        self.current_filename = None          
+        super().__init__(name_pipe)   
         
     def get_json(self):
-
+        self.profils = self.profil_paths
         self.json_outputs = []
-        audio_wave = pydub.AudioSegment.from_wav(self.current_filename)
         for segment, _, label in self.diarization.itertracks(yield_label=True):
             t1 = segment.start * 1000 #Works in milliseconds
             t2 = (segment.start+segment.duration) * 1000
-            newAudio = audio_wave[t1:t2]
+            newAudio = self.audio_profiles[t1:t2]
             newAudio.export(f"temp.wav", format="wav")
             audio_config = speechsdk.audio.AudioConfig(filename="temp.wav")
             speech_recognizer = speechsdk.SpeechRecognizer(speech_config=self.speech_config, audio_config=audio_config)
@@ -41,5 +35,13 @@ class SpeakerAward(SpeakerDiarizer):
                                       'start':time.strftime("%H:%M:%S",time.gmtime(segment.start)),
                                       'end':time.strftime("%H:%M:%S",time.gmtime(segment.start+segment.duration)),
                                       'text':result.text})
+        for count,profil in enumerate(self.profils):
+            name = profil.split("/")[-1][:-4]
+            speaker_letter = self.json_outputs[len(self.profils) - count - 1]['speaker']
+            for segment in self.json_outputs:
+                if segment['speaker'] == speaker_letter :
+                    segment['speaker'] = name
+        for i in range(len(self.profils)):
+            del self.json_outputs[i]
         os.remove("temp.wav")
         return self.json_outputs
--- msd/SpeakerDiarizer.py
+++ msd/SpeakerDiarizer.py
@@ -2,6 +2,7 @@ import torch
 from pyannote.database.util import load_rttm
 from pyannote.core import Segment, notebook
 from pyannote.metrics.diarization import DiarizationErrorRate
+import pydub
 
 class SpeakerDiarizer():
     
@@ -11,11 +12,19 @@ class SpeakerDiarizer():
         self.diarization = None
         self.der = None
         self.current_filename = None
+        self.audio_profiles = None
+        self.profil_paths = None
 
-    def apply_diarizer(self,file_name):
+    def apply_diarizer(self,file_name,profil_paths):
         # apply diarization pipeline on your audio file
-        self.diarization = self.pipeline({'audio' : file_name})
         self.current_filename = file_name
+        self.profil_paths = profil_paths
+        self.audio_profiles = pydub.AudioSegment.from_wav(self.current_filename)
+        for profil in profil_paths:
+            profil_wav = pydub.AudioSegment.from_wav(profil)    
+            self.audio_profiles = profil_wav[:] + self.audio_profiles[:]
+        self.audio_profiles.export("avecprofil.wav", format="wav")
+        self.diarization = self.pipeline({'audio' : "avecprofil.wav"})
     
     def write_rttm(self,path_outputs):
         with open(path_outputs, 'w') as f:
